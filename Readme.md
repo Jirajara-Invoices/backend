88500eda8672aa331e4af6d69da45fa36e6a0768
Adolfo Bastardo <abastardo@liberarte.org.ve>
2023-02-02 21:22:53 -0400

    [NEW] Start project scaffold

---
+++ Readme.md
@@ -0,0 +1,24 @@
+# Jirajara backend server
+
+This is the backend server for Jirajara, a web application that helps you with your
+personal accounting and business.
+
+### Requirements
+  - NodeJS 18.x
+  - Pnpm
+  - PostgreSQL (we tested with 15.1)
+  - Redis 6.x
+  - Docker
+
+# Roadmap
+  - [ ] Invoicing (products and services)
+    - [ ] Create invoice
+    - [ ] Preview invoice
+    - [ ] Send & print invoice
+    - [ ] Invoice management and dashboard
+  - [ ] Product management
+  - [ ] Point of sale (POS) system (for physical products)
+  - [ ] Customer management
+
+New features are welcome!
+    
---
+++ src/index.ts
@@ -0,0 +1,72 @@
+import compress from '@fastify/compress';
+import cors from '@fastify/cors';
+import helmet from '@fastify/helmet';
+import rateLimit from '@fastify/rate-limit';
+import Fastify from 'fastify';
+import { ApolloServer, BaseContext } from '@apollo/server';
+import fastifyApollo, { fastifyApolloDrainPlugin } from '@as-integrations/fastify';
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+# Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+# This "Book" type defines the queryable fields for every book in our data source.
+
+type Book {
+    title: String
+    author: String
+}
+
+# The "Query" type is special: it lists all of the available queries that
+# clients can execute, along with the return type for each. In this
+# case, the "books" query returns an array of zero or more Books (defined above).
+
+type Query {
+    books: [Book]
+}
+`;
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+// Resolvers define how to fetch the types defined in your schema.
+// This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const fastify = Fastify({ logger: true });
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const apollo = new ApolloServer<BaseContext>({
+  typeDefs,
+  resolvers,
+  plugins: [fastifyApolloDrainPlugin(fastify)],
+});
+
+await fastify.register(rateLimit);
+await fastify.register(helmet);
+await fastify.register(cors);
+await fastify.register(compress);
+
+await apollo.start();
+await fastify.register(fastifyApollo(apollo));
+
+const url = await fastify.listen({
+  port: 4000,
+  host: '0.0.0.0',
+});
+
+console.log(`🚀  Server ready at: ${url}`);
